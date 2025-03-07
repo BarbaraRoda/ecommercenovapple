813b8c1b053e55ad2f42fe47f64408c319375cf5
Barbara.Roda <b.roda.br94@gmail.com>
2025-03-07 15:08:57 -0300

    vercel

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+    // Desactivar reglas específicas si lo necesitas
+    dirs: ['src'], // Si tus archivos están dentro de `src`
+  },
 };
 
 export default nextConfig;
+
